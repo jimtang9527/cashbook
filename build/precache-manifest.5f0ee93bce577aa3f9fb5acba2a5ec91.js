7e303789627646014026d737bf49881b8863cd40
tang9527@aliyun.com <jim@aoma.tech>
2020-08-17 11:41:57 +0800

    完善数据完整型检测

---
+++ build/precache-manifest.5f0ee93bce577aa3f9fb5acba2a5ec91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df8f82af5e9aa624f5c3b9b9430a779",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a820c9f45d3fc233477",
+    "url": "/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "4a820c9f45d3fc233477",
+    "url": "/static/js/2.ffa8b261.chunk.js"
+  },
+  {
+    "revision": "77e2f8889e60c621dd7af5887d556f54",
+    "url": "/static/js/2.ffa8b261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e702b9a0f20304392a1a",
+    "url": "/static/js/main.31add76e.chunk.js"
+  },
+  {
+    "revision": "c6f3674c840ebc0f45dc",
+    "url": "/static/js/runtime-main.9e948e70.js"
+  }
+]);
